20844eb33c4901d0b040f94731ae322362993d7f
lee989898 <wnsrud1352@gmail.com>
2021-10-11 21:20:01 +0900

    코드 정리

--- FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -37,50 +37,7 @@ class AlbumFragment : Fragment() {
                 .commitAllowingStateLoss()
         }
 
-//        binding.albumLailacLayout.setOnClickListener{
-//            Toast.makeText(activity, "라일락", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumFluLayout.setOnClickListener{
-//            Toast.makeText(activity, "Flu", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumCoinLayout.setOnClickListener{
-//            Toast.makeText(activity, "Coin", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumSpringHelloLayout.setOnClickListener{
-//            Toast.makeText(activity, "봄 안녕", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumCelebrityLayout.setOnClickListener{
-//            Toast.makeText(activity, "Celebrity", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumRecycleLayout.setOnClickListener{
-//            Toast.makeText(activity, "돌림노래 (Feat. Dean)", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumEmptycupLayout.setOnClickListener{
-//            Toast.makeText(activity, "빈 컵 (Empty Cup)", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumKidandoceanLayout.setOnClickListener{
-//            Toast.makeText(activity, "아이와 나의 바다", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        binding.albumAhpuhLayout.setOnClickListener{
-//            Toast.makeText(activity, "어푸 (Ah puh)", Toast.LENGTH_SHORT).show()
-//        }
-//
-//
-//        binding.albumToggleOffIv.setOnClickListener {
-//            toggleStatus(true)
-//        }
-//
-//        binding.albumToggleOnIv.setOnClickListener {
-//            toggleStatus(false)
-//        }
+
 
 
 
@@ -95,18 +52,7 @@ class AlbumFragment : Fragment() {
         return binding.root
     }
 
-//    private fun toggleStatus(isSelected : Boolean){
-//        if(isSelected){
-//            binding.albumToggleOffIv.visibility = View.GONE
-//            binding.albumToggleOnIv.visibility = View.VISIBLE
-//        }
-//        else{
-//            binding.albumToggleOffIv.visibility = View.VISIBLE
-//            binding.albumToggleOnIv.visibility = View.GONE
-//
-//        }
-//
-//    }
+
 
 
 
--- FLO/app/src/main/java/com/example/flo/SongFragment.kt
+++ FLO/app/src/main/java/com/example/flo/SongFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentDetailBinding
 import com.example.flo.databinding.FragmentSongBinding
@@ -19,6 +20,64 @@ class SongFragment : Fragment() {
     ): View? {
         binding = FragmentSongBinding.inflate(inflater, container, false)
 
+        binding.songLalacLayout.setOnClickListener{
+            Toast.makeText(activity, "라일락", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.songFluLayout.setOnClickListener{
+            Toast.makeText(activity, "Flu", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.songCoinLayout.setOnClickListener{
+            Toast.makeText(activity, "Coin", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.songSpringLayout.setOnClickListener{
+            Toast.makeText(activity, "봄 안녕", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.songCelebrityLayout.setOnClickListener{
+            Toast.makeText(activity, "Celebrity", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.songSingLayout.setOnClickListener{
+            Toast.makeText(activity, "돌림노래 (Feat. Dean)", Toast.LENGTH_SHORT).show()
+        }
+
+//        binding.albumEmptycupLayout.setOnClickListener{
+//            Toast.makeText(activity, "빈 컵 (Empty Cup)", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        binding.albumKidandoceanLayout.setOnClickListener{
+//            Toast.makeText(activity, "아이와 나의 바다", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        binding.albumAhpuhLayout.setOnClickListener{
+//            Toast.makeText(activity, "어푸 (Ah puh)", Toast.LENGTH_SHORT).show()
+//        }
+
+
+        binding.songMixoffTg.setOnClickListener {
+            toggleStatus(true)
+        }
+
+        binding.songMixonTg.setOnClickListener {
+            toggleStatus(false)
+        }
+
         return binding.root
+    }
+
+    private fun toggleStatus(isSelected : Boolean){
+        if(isSelected){
+            binding.songMixoffTg.visibility = View.GONE
+            binding.songMixonTg.visibility = View.VISIBLE
+        }
+        else{
+            binding.songMixoffTg.visibility = View.VISIBLE
+            binding.songMixonTg.visibility = View.GONE
+
+        }
+
     }
 }
