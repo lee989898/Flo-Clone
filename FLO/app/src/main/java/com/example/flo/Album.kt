fd80ecc17ae59dbd89c1edd9190386da73f672ef
lee989898 <wnsrud1352@gmail.com>
2021-11-10 18:32:13 +0900

    commit

--- FLO/app/src/main/java/com/example/flo/Album.kt
+++ FLO/app/src/main/java/com/example/flo/Album.kt
@@ -1,8 +1,8 @@
 package com.example.flo
 
-class Album {
+data class Album(
     var title: String? = "",
     var singer: String? = "",
-    var coverImg : Int? = null,
-    var songs: ArrayList<String>? = null
-}
+    var coverImg: Int? = null,
+    var songs: ArrayList<Song>? = null
+)
--- FLO/app/src/main/java/com/example/flo/HomeFragment.kt
+++ FLO/app/src/main/java/com/example/flo/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.HorizontalScrollView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
@@ -13,6 +15,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
+    private var albumDates = ArrayList<Album>();
 
 
 
@@ -30,9 +33,6 @@ class HomeFragment : Fragment() {
         binding.homeSpringDotsIndicator.setViewPager2(binding.panelContentVp)
 
 
-        binding.homeTodayAlbum01Iv.clipToOutline = true
-        binding.homeTodayAlbum02Iv.clipToOutline = true
-        binding.homeTodayAlbum03Iv.clipToOutline = true
         binding.homePotcast01Iv.clipToOutline = true
         binding.homePotcast02Iv.clipToOutline = true
         binding.homePotcast03Iv.clipToOutline = true
@@ -40,11 +40,28 @@ class HomeFragment : Fragment() {
         binding.homeVideo02Iv.clipToOutline = true
         binding.homeViewpagerExp2Iv.clipToOutline = true
 
-        binding.homeTodayAlbum01Iv.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.main_frm, AlbumFragment())
-                .commitAllowingStateLoss()
+//        binding.homeTodayAlbum01Iv.setOnClickListener {
+//            (context as MainActivity).supportFragmentManager.beginTransaction()
+//                .replace(R.id.main_frm, AlbumFragment())
+//                .commitAllowingStateLoss()
+//        }
+
+        // 데이터 리스트 생성 더미 데이터
+        albumDates.apply {
+            add(Album("Butter", "방탄소년단(BTS)", R.drawable.img_album_exp2))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp2))
+            add(Album("Boy with Luv", "방탄소년단(BTS)", R.drawable.img_album_exp2))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp2))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp2))
         }
+
+        // 더미데이터랑 Adapter 연결
+        val albumRVAdapter = AlbumRVAdapter(albumDates)
+        // 리사이클러뷰에 어댑터를 연결
+        binding.homeTodayMusicAlbumRecycleerView.adapter = albumRVAdapter
+        // 레이아웃 매니저 설정
+        binding.homeTodayMusicAlbumRecycleerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
 
         val bannerAdapter = BannerViewpagerAdapter(this)
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
