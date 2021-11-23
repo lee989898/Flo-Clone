55c11241bc3b4df78183d6b4abfa3d930c8c6ded
lee989898 <wnsrud1352@gmail.com>
2021-11-23 19:00:58 +0900

    commit

---
+++ umc_android_flo-2f14074fd012c1a537a7ebd6c70138a616a32d01/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -0,0 +1,67 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
+
+class AlbumFragment : Fragment() {
+
+    lateinit var binding: FragmentAlbumBinding
+    private var gson: Gson = Gson()
+
+    val information = arrayListOf("수록곡", "상세정보", "영상")
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        val albumData = arguments?.getString("album")
+        val album = gson.fromJson(albumData, Album::class.java)
+
+        setViews(album)
+        //ROOM_DB
+        val songs = getSongs(album.id) //앨범안에 있는 수록곡들을 불러옵니다.
+        // 이 다음에 수록곡 프래그먼트에 songs을 전달해주는 식으로 사용하시면 됩니다.
+
+        //set click listener
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frm, HomeFragment())
+                    .commitAllowingStateLoss()
+        }
+
+        //init viewpager
+        val albumAdapter = AlbumVPAdapter(this)
+
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp){
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+        return binding.root
+    }
+
+    private fun setViews(album: Album) {
+        binding.albumMusicTitleTv.text = album.title.toString()
+        binding.albumSingerNameTv.text = album.singer.toString()
+        binding.albumAlbumIv.setImageResource(album.coverImg!!)
+    }
+
+    //ROOM_DB
+    private fun getSongs(albumIdx: Int): ArrayList<Song>{
+        val songDB = SongDatabase.getInstance(requireContext())!!
+
+        val songs = songDB.songDao().getSongsInAlbum(albumIdx) as ArrayList
+
+        return songs
+    }
+}
---
+++ umc_android_flo-2f14074fd012c1a537a7ebd6c70138a616a32d01/app/src/main/java/com/example/flo/LockerFragment.kt
@@ -0,0 +1,40 @@
+package com.example.flo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class LockerFragment : Fragment() {
+
+    lateinit var binding: FragmentLockerBinding
+    private val information = arrayListOf("저장한 곡", "음악파일")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerAdapter = LockerViewpagerAdapter(this)
+        binding.lockerContentVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+        binding.lockerLoginTv.setOnClickListener {
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+
+        return binding.root
+    }
+
+}
